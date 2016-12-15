b75f6e340330d83c3edd901ed5cb37fb0937f197
Leandro Chaves <leandro@chaves.in>
2016-12-15 16:13:54 -0200

    Initial Configuration.

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2016 Estandarte. All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Guigorigo/theme',
+    __DIR__
+);
